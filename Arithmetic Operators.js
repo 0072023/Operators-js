428b52a1c6f24cd6d7813bd3068eb577e73b65dc
Vitor_Augusto_Rodrigues <131215606+0072023@users.noreply.github.com>
2024-09-22 15:03:11 -0300

    Add files via upload

---
+++ Arithmetic Operators.js
@@ -0,0 +1,41 @@
+//Arithmetic Operators = operands(values,variables,etc.)
+//                       operators(+ - * /)
+//                       ex. 11 = x + 5;
+
+//let students = 30;
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2(pot);
+
+//students += 1;
+//students -= 1;
+//students *= 1;
+//students /= 1;
+//students **= 1;
+
+//students++;
+//students--;
+
+//console.log(students)
+
+    /*
+        Operators precedence
+            1.parenthesis ()
+            2.exponents
+            3.multiplication & division & modulo
+            4.addition & subtraction
+    */
+
+            //let result = 1 + 2 * 3 + 4 ** 2;l
+            //let result = 1 + 6 + 16;
+            //let result = 23;
+            //console.log(result)
+
+            //let result = 12 % 5 + 8 / 2;
+            //let result = 6;
+            //console.log(result);
+
+            let result = 6 / 2 ** (2 + 5);
+            console.log(result)
